5b8eb16cb34fd68983e20575efad90714760affa
neorepo <neorepo.edu@gmail.com>
2021-02-13 21:02:44 -0300

    Últimando detalles

--- localidades_arg_csv.php
+++ localidades_arg_csv.php
@@ -55,32 +55,31 @@ if (file_exists(DOCUMENT_ROOT)) {
 // Creamos el directorio nuevamente
 mkdir(DOCUMENT_ROOT);
 
+$headerHttp = [
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0",
+    "Accept: application/json, text/javascript, */*; q=0.01",
+    "Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Content-Type: application/x-www-form-urlencoded; charset=UTF-8",
+    "X-Requested-With: XMLHttpRequest"
+];
+
 foreach ($provincias as $key => $value) {
-    $curlData = 'action=localidades&localidad=none&calle=&altura=&provincia=' . $key;
     // https://www.php.net/manual/es/function.curl-setopt.php
     $options = [
         // CURLOPT_HTTPHEADER Un array de campos a configurar para el header HTTP, en el formato: array('Content-type: text/plain', 'Content-length: 100')
-        CURLOPT_HTTPHEADER => [
-            "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0",
-            "Accept: application/json, text/javascript, */*; q=0.01",
-            "Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Content-Type: application/x-www-form-urlencoded; charset=UTF-8",
-            "X-Requested-With: XMLHttpRequest"
-        ],
+        CURLOPT_HTTPHEADER => $headerHttp,
         // true para completar silenciosamente en lo que se refiere a las funciones cURL, equivalente a curl -s (silent mode).
         CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
+        CURLOPT_ENCODING => "", // Todas
         CURLOPT_POST => 1,
         // Si pasamos un array a CURLOPT_POSTFIELDS codificará los datos como multipart/form-data, 
         // pero si pasamos una cadena URL-encoded codificará los datos como application/x-www-form-urlencoded. 
-        CURLOPT_POSTFIELDS => $curlData,
-        CURLOPT_VERBOSE => 1 // Para ver qué sucede en detalle
+        CURLOPT_POSTFIELDS => 'action=localidades&localidad=none&calle=&altura=&provincia=' . $key,
+        CURLOPT_VERBOSE => 1 // Muestra en detalle lo que esta sucediendo.
     ];
     
-    $url = 'https://www.correoargentino.com.ar/sites/all/modules/custom/ca_forms/api/wsFacade.php';
-    $curl = curl_init($url);
+    $curl = curl_init('https://www.correoargentino.com.ar/sites/all/modules/custom/ca_forms/api/wsFacade.php');
     curl_setopt_array($curl, $options);
-    // https://www.iteramos.com/pregunta/31833/php-como-quitar-todos-los-no-imprimibles-de-caracteres-en-una-cadena
     // https://alvinalexander.com/php/how-to-remove-non-printable-characters-in-string-regex/
     // $regex = '/[\x00-\x1F\x80-\xFF]/';
     $response = preg_replace('/[[:^print:]]/', '', curl_exec($curl));
@@ -98,5 +97,4 @@ foreach ($provincias as $key => $value) {
 }
 
 $time = microtime(true) - $time;
-echo "<p>Mission Complete!!! :)\n";
 echo "<p>Tiempo total de ejecución: " . round($time, 3) . " segundos.";
--- localidades_arg_json.php
+++ localidades_arg_json.php
@@ -55,31 +55,32 @@ if (file_exists(DOCUMENT_ROOT)) {
 // Creamos el directorio
 mkdir(DOCUMENT_ROOT);
 
+//
+$headerHttp = [
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0",
+    "Accept: application/json, text/javascript, */*; q=0.01",
+    "Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
+    "Content-Type: application/x-www-form-urlencoded; charset=UTF-8",
+    "X-Requested-With: XMLHttpRequest"
+];
+
 foreach ($provincias as $key => $value) {
-    $data = 'action=localidades&localidad=none&calle=&altura=&provincia=' . $key;
     $response = '{"iso_31662":"AR-' . $key . '","provincia":"'. $provincias[$key] . '","localidades":';
     // https://www.php.net/manual/es/function.curl-setopt.php
     $options = [
         // CURLOPT_HTTPHEADER Un array de campos a configurar para el header HTTP, en el formato: array('Content-type: text/plain', 'Content-length: 100')
-        CURLOPT_HTTPHEADER => [
-            "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0",
-            "Accept: application/json, text/javascript, */*; q=0.01",
-            "Accept-Language: es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
-            "Content-Type: application/x-www-form-urlencoded; charset=UTF-8",
-            "X-Requested-With: XMLHttpRequest"
-        ],
+        CURLOPT_HTTPHEADER => $headerHttp,
         // true para completar silenciosamente en lo que se refiere a las funciones cURL, equivalente a curl -s (silent mode).
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => "",
         CURLOPT_POST => 1,
         // Si pasamos un array a CURLOPT_POSTFIELDS codificará los datos como multipart/form-data, 
         // pero si pasamos una cadena URL-encoded codificará los datos como application/x-www-form-urlencoded. 
-        CURLOPT_POSTFIELDS => $data,
-        CURLOPT_VERBOSE => 1
+        CURLOPT_POSTFIELDS => 'action=localidades&localidad=none&calle=&altura=&provincia=' . $key,
+        CURLOPT_VERBOSE => 1 // Muestra en detalle lo que esta sucediendo.
     ];
-    
-    $url = 'https://www.correoargentino.com.ar/sites/all/modules/custom/ca_forms/api/wsFacade.php';
-    $curl = curl_init($url);
+
+    $curl = curl_init('https://www.correoargentino.com.ar/sites/all/modules/custom/ca_forms/api/wsFacade.php');
     curl_setopt_array($curl, $options);
     // https://alvinalexander.com/php/how-to-remove-non-printable-characters-in-string-regex/
     // $regex = '/[\x00-\x1F\x80-\xFF]/'; // '/[^\x20-\x7E]/';
